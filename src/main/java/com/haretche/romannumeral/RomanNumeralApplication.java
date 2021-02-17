7e35afaf992820d2cf9060a26fef13348cea6227
Santiago Haretche <santiago.haretche@gmail.com>
2021-02-16 22:11:07 -0800

    Java Doc comment for Main Application class

--- src/main/java/com/haretche/romannumeral/RomanNumeralApplication.java
+++ src/main/java/com/haretche/romannumeral/RomanNumeralApplication.java
@@ -3,6 +3,9 @@ package com.haretche.romannumeral;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Application Main class.
+ */
 @SpringBootApplication
 public class RomanNumeralApplication {
 
